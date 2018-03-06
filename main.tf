944ad94954719c4d6de19be145989c6c532c7d07
Paul Towers <paultowers@Pauls-MacBook-Pro.local>
2018-03-06 11:46:52 -0500

    added elb

--- main.tf
+++ main.tf
@@ -4,13 +4,15 @@ provider "aws" {
 
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
-  default = 8080
+  default     = 8080
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-40d28157"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.http.id}"]
+data "aws_availability_zones" "all" {}
+
+resource "aws_launch_configuration" "launchconfig1" {
+  image_id        = "ami-40d28157"
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.http.id}"]
 
   user_data = <<-EOF
     #!/bin/bash
@@ -18,8 +20,8 @@ resource "aws_instance" "example" {
     nohup busybox httpd -f -p "${var.server_port}" &
     EOF
 
-  tags {
-    Name = "ubuntu-test"
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -32,8 +34,66 @@ resource "aws_security_group" "http" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "http-elb-sg" {
+  name = "http-elb-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_autoscaling_group" "asg1" {
+  launch_configuration = "${aws_launch_configuration.launchconfig1.id}"
+  min_size             = 2
+  max_size             = 10
+  availability_zones   = ["${data.aws_availability_zones.all.names}"]
+  load_balancers       = ["${aws_elb.http-elb.name}"]
+  health_check_type    = "ELB"
+
+  tag {
+    key                 = "Name"
+    value               = "http-webgroup"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_elb" "http-elb" {
+  name               = "http-elb1"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  security_groups    = ["${aws_security_group.http-elb-sg.id}"]
+
+  listener {
+    lb_port           = 80
+    lb_protocol       = "http"
+    instance_port     = "${var.server_port}"
+    instance_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:${var.server_port}/"
+    interval            = 30
+  }
 }
 
 output "public_ip" {
-  value = "${aws_instance.example.public_ip}"
+  value = "${aws_elb.http-elb.dns_name}"
 }
